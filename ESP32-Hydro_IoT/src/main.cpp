3dd84645770dd0e6ccb608891b6739c50ba2e7da
Rohman Aditiya <74638607+rohmxx@users.noreply.github.com>
2022-08-08 19:06:26 +0700

    Update main.cpp

--- ESP32-Hydro_IoT/src/main.cpp
+++ ESP32-Hydro_IoT/src/main.cpp
@@ -12,15 +12,15 @@
 #define adc_res 4096
 #define samplerate 30
 
-#define ppm_ph_pin 2
-#define ph_pump 13
-#define mixed_pump 14
-#define plant_pump 27
-#define nutrisi_pump 12
+#define ppm_pin 4
+#define ph_pin 2
+#define otolevel_radar 2
+#define aerator_pump 14
+#define solenoid 33
+#define ph_pump 32
+#define nutrisia_pump 27
+#define nutrisib_pump 19
 #define pilot_lamp 25
-#define ppm_ph_switch 26
-#define otolevel_radar 33
-#define nutrisi_optional 32
 #define lcd_addr 0x27
 
 #define DHTPIN 15
@@ -58,6 +58,9 @@ uint timestamp;
 
 uint8_t i,j,k;
 uint8_t sens_mode = 0; // 0 is ppm; 1 is ph;
+uint8_t ppm_state = 0;
+uint8_t ph_state = 0;
+uint8_t water_state = 0;
 
 uint8_t temperature = 0;
 uint8_t humidity = 0;
@@ -80,7 +83,8 @@ unsigned long sendDataPrevMillis = 0;
 unsigned long readDataPrevMillis = 0;
 unsigned long nutripumpMillis = 0;
 unsigned long phpumpMillis = 0;
-unsigned long mixpumpMillis = 0;
+unsigned long processMillis = 0;
+unsigned long timenow = 0;
 
 void initWiFi() {
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
@@ -119,6 +123,7 @@ void readFirebase(){
   setp_hari[4]  = Firebase.RTDB.getInt(&fbdo, F("/Setpoint/5/hari")) ? fbdo.to<int>() : 0;
   setp_ppm[4]   = Firebase.RTDB.getInt(&fbdo, F("/Setpoint/5/ppm")) ? fbdo.to<int>() : 0;
   setp_ph[4]    = Firebase.RTDB.getInt(&fbdo, F("/Setpoint/5/ph")) ? fbdo.to<int>() : 0;
+  day_count     =  Firebase.RTDB.getInt(&fbdo, F("/Day_Counter")) ? fbdo.to<int>() : 0;
 }
 
 void sendFirebase(){
@@ -142,132 +147,165 @@ void read_DHT11(){
 }
 
 void read_ppm_ph(){
-  // read ppm
-  if(sens_mode == 0){
-    for (i=0;i<samplerate;i++){
-      ppm_adc+= analogRead(ppm_ph_pin);
-    }
-    ppm_data = 7 + ( ( 2.5 - (3.3/adc_res*ppm_adc/samplerate) ) / 0.18 );
-    sens_mode = 1;
-    digitalWrite(ppm_ph_switch, 1);
-  }
-  // read ph
-  else if(sens_mode == 1){
-    for(i=0;i<samplerate;i++){
-      ph_adc += analogRead(ppm_ph_pin);
-    }
-    ph_data = 7 + ( ( 2.5 - (3.3/adc_res*ph_adc/samplerate) ) / 0.18 );
-    sens_mode = 0;
-    digitalWrite(ppm_ph_switch, 0);
-  }
+  // // read ppm
+  // if(sens_mode == 0){
+  //   ppm_adc = analogRead(ppm_ph_pin);
+  //   ppm_data = (0.3417*ppm_adc)+281.08;
+  //   sens_mode = 1;
+  //   digitalWrite(ppm_ph_switch, 1);
+  // }
+  // // read ph
+  // else if(sens_mode == 1){
+  //   for(i=0;i<samplerate;i++){
+  //     ph_adc += analogRead(ppm_ph_pin);
+  //   }
+  //   ph_data = 7 + ( ( 2.5 - (3.3/adc_res*ph_adc/samplerate) ) / 0.18 );
+  //   sens_mode = 0;
+  //   digitalWrite(ppm_ph_switch, 0);
+  // }
 }
 
 void process_ph(){
-  // proses campuran ph
+  if((int)ph_data < sp_ph){
+    if(ph_state==1){
+      if((timenow - phpumpMillis) > 5000){
+        ph_state=0;
+        phpumpMillis=timenow;
+      }
+    }
+    else if(ph_state==0){
+      if((timenow - phpumpMillis) > 1000){
+        ph_state=1;
+        phpumpMillis=timenow;
+      }
+    }
+    digitalWrite(ph_pump, ph_state);
+  }
 }
 
 void process_ppm(){
-  // proses campuran ppm
+  if((int)ppm_data < sp_ppm){
+    if(ppm_state==1){
+      if((timenow - nutripumpMillis) > 5000){
+        ppm_state=0;
+        nutripumpMillis=timenow;
+      }
+    }
+    else if(ppm_state==0){
+      if((timenow - nutripumpMillis) > 1000){
+        ppm_state=1;
+        nutripumpMillis=timenow;
+      }
+    }
+    digitalWrite(nutrisia_pump, ppm_state);
+    digitalWrite(nutrisib_pump, ppm_state);
+  }
 }
 
 void setup()
 {
   Serial.begin(115200);
-  dht.begin();
-  lcd.init();
-  lcd.backlight();
-  initWiFi();
-  timeClient.begin();
+  // dht.begin();
+  // lcd.init();
+  // lcd.backlight();
+  // initWiFi();
+  // timeClient.begin();
 
-  pinMode(32, OUTPUT);
-  pinMode(25, OUTPUT);
-  pinMode(27, OUTPUT);
+  pinMode(aerator_pump, OUTPUT);
+  pinMode(ph_pump, OUTPUT);
+  pinMode(nutrisia_pump, OUTPUT);
+  pinMode(nutrisib_pump, OUTPUT);
+
+  digitalWrite(aerator_pump, 1);
+  digitalWrite(ph_pump, 1);
+  digitalWrite(nutrisia_pump, 1);
+  digitalWrite(nutrisib_pump, 1);
   
-//  auth.user.email = USER_EMAIL;
-//  auth.user.password = USER_PASSWORD;
-  config.api_key = API_KEY;
-  config.database_url = DATABASE_URL;
+// //  auth.user.email = USER_EMAIL;
+// //  auth.user.password = USER_PASSWORD;
+//   config.api_key = API_KEY;
+//   config.database_url = DATABASE_URL;
 
-  lcd.clear();
-  lcd.setCursor(0,0);
-  lcd.print("Connecting Firebase");
-  if(Firebase.signUp(&config, &auth, "", "")){
-    Serial.println("ok");
-    lcd.setCursor(0,1);
-    lcd.print("ok");
-    signupOK = true;
-  }
-  else{
-    Serial.printf("%s\n", config.signer.signupError.message.c_str());
-    lcd.setCursor(0,1);
-    lcd.print("error");
-  }
+//   lcd.clear();
+//   lcd.setCursor(0,0);
+//   lcd.print("Connecting Firebase");
+//   if(Firebase.signUp(&config, &auth, "", "")){
+//     Serial.println("ok");
+//     lcd.setCursor(0,1);
+//     lcd.print("ok");
+//     signupOK = true;
+//   }
+//   else{
+//     Serial.printf("%s\n", config.signer.signupError.message.c_str());
+//     lcd.setCursor(0,1);
+//     lcd.print("error");
+//   }
   
-  Firebase.reconnectWiFi(true);
-  fbdo.setResponseSize(4096);
-  config.token_status_callback = tokenStatusCallback;
-  config.max_token_generation_retry = 5;
-  Firebase.begin(&config, &auth);
+//   Firebase.reconnectWiFi(true);
+//   fbdo.setResponseSize(4096);
+//   config.token_status_callback = tokenStatusCallback;
+//   config.max_token_generation_retry = 5;
+//   Firebase.begin(&config, &auth);
 
-  // Getting the user UID might take a few seconds
-  Serial.println("Getting User UID");
-  while ((auth.token.uid) == "") {
-    Serial.print('.');
-    delay(1000);
-  }
-  uid = auth.token.uid.c_str();
-  Serial.print("User UID: ");
-  Serial.println(uid);
-  lcd.clear();
-
-  digitalWrite(ppm_ph_switch, 0);
+//   // Getting the user UID might take a few seconds
+//   Serial.println("Getting User UID");
+//   while ((auth.token.uid) == "") {
+//     Serial.print('.');
+//     delay(1000);
+//   }
+//   uid = auth.token.uid.c_str();
+//   Serial.print("User UID: ");
+//   Serial.println(uid);
+//   lcd.clear();
 }
 
 void loop()
 {
-  // Read setpoint and sensor every 1second
-  if(Firebase.ready() && (millis() - readDataPrevMillis > 1000 || readDataPrevMillis == 0)){
-    sendDataPrevMillis = millis();
-    readFirebase();
-    read_DHT11();
-    read_ppm_ph();
-  }
+  timenow = millis();
+  // // Read setpoint and sensor every 1second
+  // if(Firebase.ready() && (timenow - readDataPrevMillis > 1000 || readDataPrevMillis == 0)){
+  //   sendDataPrevMillis = millis();
+  //   readFirebase();
+  // }
 
-  // Send to database every 1minute
-  if(Firebase.ready() && (millis() - sendDataPrevMillis > 60000 || sendDataPrevMillis == 0)){
-    sendDataPrevMillis = millis();
-    sendFirebase();
-  }
+  // // Send to database every 1minute
+  // if((timenow - sendDataPrevMillis > 30000 || sendDataPrevMillis == 0)){
+  //   sendDataPrevMillis = millis();
+  //   sendFirebase();
+  // }
 
-  //Automation Process
-  for(j = 0; j < 5; i++){  //get setpoint from daycount
-    if(day_count>=setp_hari[i]){
-      sp_ph = setp_ph[i];
-      sp_ppm = setp_ppm[i];
-      break;
-    }
+  // // Automation Process
+  // for(j = 0; j < 5; i++){  //get setpoint from daycount
+  //   if(day_count>=setp_hari[i]){
+  //     sp_ph = setp_ph[i];
+  //     sp_ppm = setp_ppm[i];
+  //     break;
+  //   }
+  // }
+  if((timenow - processMillis) > 1000 && (timenow - processMillis) <= 6000){
+    digitalWrite(aerator_pump, 0);
+    digitalWrite(nutrisia_pump, 1);
+    digitalWrite(nutrisib_pump, 1);
+    digitalWrite(ph_pump, 1);
   }
-  if(ph_data<sp_ph){
-    process_ph();
-  }
-  if(ppm_data<sp_ppm){
+  else if(timenow - processMillis > 6000 && (timenow - processMillis) <= 26000){
+    digitalWrite(aerator_pump, 0);
+    digitalWrite(ph_pump, 1);
     process_ppm();
   }
-  // if(ppm<setp){
-  //   if(state==1){
-  //     if((millis() - prevtime) > 5000){
-  //       state=0;
-  //       prevtime=millis();
-  //     }
-  //   }
-  //   else if(state==0){
-  //     if((millis() - prevtime) > 1000){
-  //       state=1;
-  //       prevtime=millis();
-  //     }
-  //   }
-  //   digitalWrite(32, state);
-  //   digitalWrite(25, state);
-  //   digitalWrite(27, state);
-  // }
+  else if(timenow - processMillis > 26000 && (timenow - processMillis) <= 46000){
+    digitalWrite(aerator_pump, 0);
+    digitalWrite(nutrisia_pump, 1);
+    digitalWrite(nutrisib_pump, 1);
+    process_ph();
+  }
+  else if(timenow - processMillis > 46000 && (timenow - processMillis) <= 56000){
+    digitalWrite(aerator_pump, 1);
+    digitalWrite(nutrisia_pump, 1);
+    digitalWrite(nutrisib_pump, 1);
+    digitalWrite(ph_pump, 1);
+  }
+  else if(timenow - processMillis > 56000){
+    processMillis = timenow;
+  }
 }
